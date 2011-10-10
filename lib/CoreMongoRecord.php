c4c91c1ec9fbe7d8ad9a4866acce48a4859a92d5
Ryan Schumacher <j.r.schumacher@gmail.com>
2011-10-10 10:31:05 -0500

    Fix anonymous MongoRecord feature
    - Numerious bugs were fixed
    - Fully creates and finds records

--- lib/CoreMongoRecord.php
+++ lib/CoreMongoRecord.php
@@ -1,5 +1,24 @@
 <?php
+  /**
+   * Mongo Record, a simple Mongo ORM for PHP with ActiveRecord-like features.
+   * 
+   * @author     Ryan Schumacher <https://github.com/jrschumacher/>
+   * @author     Lu Wang <https://github.com/lunaru/>
+   * @license    See LICENSE
+   * @version    1.5
+   */
+
+  namespace MongoRecord;
   
+  /**
+   * Core Mongo Record, core functionality Mongo Record library
+   * 
+   * This class is used to define core functionality, including opening a conn-,
+   * ection to the server, connecting to the database, and selecting the coll-
+   * ection.
+   * 
+   * @package    MongoRecord
+   */
   class CoreMongoRecord {
     
     /**
@@ -32,7 +51,7 @@
      * @param mixed $connection the connection variable either Mongo resource or mongo DSN
      * @param array $options as defined in the link above
      */
-    public static function mongoSetConnection($connection = '', $options = array('connect' => FALSE)) {
+    public static function mongoSetConnection($connection = 'localhost', $options = array('connect' => FALSE)) {
       // If is an existing connection
       if(is_object($connection) && get_class($connection) == 'Mongo') {
         // Set connection if connected
@@ -43,8 +62,8 @@
         
         // Try to connect to connection
         try {
-          $connection->connect();
-          $connection->close();
+          self::$connection->connect();
+          self::$connection->close();
         }
         catch(MongoException $e) {
           throw new CoreMongoRecordException("Could not connect to Mongo server, $connection: {$e->getMessage()}");
@@ -53,7 +72,7 @@
       
       // Connect based on given dsn
       try {
-        self::$connection = new Mongo($connection, $options);
+        self::$connection = new \Mongo($connection, $options);
       }
       catch(MongoException $e) {
         throw new CoreMongoRecordException("Could not connect to Mongo server, $connection: {$e->getMessage()}");
@@ -93,7 +112,7 @@
      */
     protected static function mongoIsAnonymous() {
       $class_name = get_called_class();
-      if($class_name === 'MongoRecord') {
+      if($class_name === 'MongoRecord\MongoRecord') {
         return TRUE;
       }
       return FALSE;
@@ -135,22 +154,20 @@
         $collection_name = $inflector->tableize($collection_name);
     
         if(self::$database == NULL) {
-          throw new Exception("BaseMongoRecord::database must be initialized to a proper database string");
+          throw new Exception("CoreMongoRecord::database must be initialized to a proper database string");
         }
     
         if(self::$connection == NULL) {
-          throw new Exception("BaseMongoRecord::connection must be initialized to a valid Mongo object");
+          throw new Exception("CoreMongoRecord::connection must be initialized to a valid Mongo object");
         }
         
         if(!(self::$connection->connected)) {
           self::$connection->connect();
         }
         
-        $collection =& self::$connection->selectCollection(self::$database, $collection_name);
+        $collection = self::$connection->selectCollection(self::$database, $collection_name);
       }
   
       return $collection;
     }    
   }
-  
-?>
--- lib/MongoRecordExceptions.php
+++ lib/MongoRecordExceptions.php
@@ -1,4 +1,15 @@
 <?php
+  /**
+   * Mongo Record, a simple Mongo ORM for PHP with ActiveRecord-like features.
+   * 
+   * @author     Ryan Schumacher <https://github.com/jrschumacher/>
+   * @author     Lu Wang <https://github.com/lunaru/>
+   * @license    See LICENSE
+   * @version    1.5
+   */
 
-  class CoreMongoRecordException extends Exception {}
-  class BaseMongoRecordException extends Exception {}
+  namespace MongoRecord;
+
+  class CoreMongoRecordException extends \Exception {}
+  class BaseMongoRecordException extends \Exception {}
+  class MongoRecordException extends \Exception {}
